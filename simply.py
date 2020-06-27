109ffe4888cc34e70493742e8561e9e39ddb4bf5
tech force <misbah4064@gmail.com>
2020-06-27 18:04:05 -0500

    added main files

---
+++ simply.py
@@ -0,0 +1,10 @@
+import aiml
+
+kernel = aiml.Kernel()
+kernel.learn("std-startup.xml")
+kernel.respond("load aiml b")
+
+while True:
+    input_text = input(">Human: ")
+    response = kernel.respond(input_text)
+    print(">Bot: "+response)
